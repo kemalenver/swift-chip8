569604d341e9e5450a265105f6e10e4e23f263ca
Kemal Enver <kemal.enver@qantasloyalty.com>
2019-03-25 13:56:40 +1100

    Initial Commit

---
+++ CHIP8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CHIP8
+//
+//  Created by Kemal on 25/3/19.
+//  Copyright © 2019 Kemal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
